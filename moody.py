ad160214921aa65e17816622b35391b705ec3e30
Agnė Klimaitė <agnaite@me.com>
2017-03-17 17:11:09 -0700

    refactored moody

--- moody.py
+++ moody.py
@@ -6,17 +6,18 @@ chatbot = ChatBot(
     trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
 )
 
-chatbot.train("chatterbot.corpus.english")
-chatbot.train("chatterbot.corpus.english.greetings")
-chatbot.train("chatterbot.corpus.english.conversations")
-# chatbot.train("chatterbot.corpus.english.swear_words")
-chatbot.train("chatterbot.corpus.english.emotions")
 
-chatbot.set_trainer(ListTrainer)
+def train_with_corpus(corpus):
+    """Trains using json corpi"""
+
+    chatbot.set_trainer("chatterbot.trainers.ChatterBotCorpusTrainer")
+    chatbot.train(corpus)
 
 
 def train_andy(question, answer):
     """Train the bot with phrase pairs"""
+
+    chatbot.set_trainer(ListTrainer)
 
     chatbot.train([
         question,
